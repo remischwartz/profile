fab4d8af63344d0fe356b167185142a0ee3d5633
Rémi Schwartz <remi.schwartz@fabernovel.com>
2025-07-20 17:07:17 +0200

    hero and github action

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -10,8 +10,8 @@ export const MobileNav = ({ children }: { children: React.ReactNode }) => {
   return (
     // <PopoverGroup>
     <Popover className="relative md:hidden">
-      <PopoverBackdrop className="fixed inset-x-0 top-12 bottom-0 bg-black/50 z-30" />
-      <PopoverButton className="px-4 py-3 font-semibold text-lg">
+      <PopoverBackdrop className="fixed inset-x-0 top-12 bottom-0 z-30 bg-black/50" />
+      <PopoverButton className="px-4 py-3 text-lg font-semibold">
         Menu ☰
       </PopoverButton>
       <PopoverPanel
@@ -19,7 +19,7 @@ export const MobileNav = ({ children }: { children: React.ReactNode }) => {
         transition
         modal
         focus
-        className=" w-full flex flex-col gap-6 bg-surface z-50 px-4 py-6 transition duration-200 ease-out data-closed:scale-95 data-closed:opacity-0"
+        className="bg-surface z-50 flex w-full flex-col gap-6 px-4 py-6 transition duration-200 ease-out data-closed:scale-95 data-closed:opacity-0"
       >
         {children}
       </PopoverPanel>
